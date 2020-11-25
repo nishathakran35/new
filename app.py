bafd54644f820d46d66533336fb6b791fd6fce70
nishathakran35 <nishathakran35@gmail.com>
2020-11-25 16:26:57 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,request
+from flask_restful import Resource,Api
+from flask_jwt import JWT
+
+from user import UserRegister
+from item import Student
+from security import authenticate,identity
+
+app=Flask(__name__)
+app.secret_key="123@123"
+api=Api(app)
+
+#items=[]
+jwt = JWT(app, authenticate, identity)
+		
+		
+api.add_resource(Student,'/student/<string:name>')
+api.add_resource(UserRegister, '/register')
+app.run()
+		
+	
